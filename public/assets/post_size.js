41c3df245a44be0874dac939f90ef4ec8a95836c
lepa15 <lepa2604@gamil.com>
2025-11-08 01:04:31 +0200

    Добавил функцию подсчета количества символов в посте

--- public/assets/post_size.js
+++ public/assets/post_size.js
@@ -1,4 +1,4 @@
-function postSize(post) {
+export default function postSize(post) {
     const urlRegex = /\b(?:https?:\/\/|www\.|t\.me\/)?(?:[\w-]+\.)+[a-z]{2,}(?:\/[^\s.,!?;:]*)?(?:\?[^\s.,!?;:]*)?(?:#[^\s.,!?;:]*)?/gi;
     const matches = post.match(urlRegex) || [];
     const urlsLength = matches.reduce((sum, url) => sum + url.length, 0);
@@ -6,4 +6,3 @@ function postSize(post) {
     return post.length - urlsLength + matches.length;
 }
 
-export default postSize;
